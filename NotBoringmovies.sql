76ea19f0d22c1bcaa476ad9d02726ce328991e0d
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-06-19 16:34:09 +0530

    Day 1

---
+++ NotBoringmovies.sql
@@ -0,0 +1,30 @@
+-- SELECT *
+-- FROM Cinema
+-- WHERE id % 2 != 0 AND description != 'boring'
+-- ORDER BY rating desc;
+select *
+from Cinema 
+where 
+MOD(id,2)=1
+AND
+description<>'boring'--inequality
+order by
+rating
+desc;
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+Output: 
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
---
+++ RisingTemperature.sql
@@ -0,0 +1,22 @@
+
+select
+w1.id from Weather w1,Weather w2
+where w1.temperature>w2.temperature
+and
+w1.recordDate=DATE_ADD(w2.recordDate,INTERVAL 1 DAY);
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
+-- +----+
