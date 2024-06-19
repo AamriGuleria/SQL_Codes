220b4c09c45908d1a4133af75464f76c72ff7f1f
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-06-19 17:26:25 +0530

    Day 1

---
+++ SecondHighestSalary.sql
@@ -0,0 +1,39 @@
+select 
+IFNULL(
+(select distinct salary
+from Employee
+order by salary
+desc
+limit 1 OFFSET 1 ),NULL) AS SecondHighestSalary;
+-- Example 1:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+-- Example 2:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | null                |
+-- +---------------------+
