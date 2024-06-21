5d7c473c5eb7bde323afbf1efa6c589b8ea8d0c0
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-06-21 17:40:53 +0530

    Day 4

---
+++ ManagerWithAtLeastFiveDirectReports.sql
@@ -0,0 +1,25 @@
+-- Input: 
+-- Employee table:
+-- +-----+-------+------------+-----------+
+-- | id  | name  | department | managerId |
+-- +-----+-------+------------+-----------+
+-- | 101 | John  | A          | null      |
+-- | 102 | Dan   | A          | 101       |
+-- | 103 | James | A          | 101       |
+-- | 104 | Amy   | A          | 101       |
+-- | 105 | Anne  | A          | 101       |
+-- | 106 | Ron   | B          | 101       |
+-- +-----+-------+------------+-----------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | John |
+-- +------+
+select name
+from Employee
+join
+(select count(*) as number,managerId from Employee group by managerId) table1
+on
+Employee.id=table1.managerId and table1.number>=5
+;
