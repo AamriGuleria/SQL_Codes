aa40382aa823f79dd512fa6c55ae6addfe56b949
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-06-27 13:47:21 +0530

    Day 9

---
+++ CustomerWhoBoughtAllCars.sql
@@ -0,0 +1,37 @@
+select t1.customer_id
+from
+(
+select
+customer_id,count(distinct product_key) as c
+from
+Customer
+group by
+customer_id
+) t1
+where t1.c=(select count(distinct product_key) from Product) 
+;
+-- Input: 
+-- Customer table:
+-- +-------------+-------------+
+-- | customer_id | product_key |
+-- +-------------+-------------+
+-- | 1           | 5           |
+-- | 2           | 6           |
+-- | 3           | 5           |
+-- | 3           | 6           |
+-- | 1           | 6           |
+-- +-------------+-------------+
+-- Product table:
+-- +-------------+
+-- | product_key |
+-- +-------------+
+-- | 5           |
+-- | 6           |
+-- +-------------+
+-- Output: 
+-- +-------------+
+-- | customer_id |
+-- +-------------+
+-- | 1           |
+-- | 3           |
+-- +-------------+
