f1d8a2c313e20495e5ec57c024d92192ac0af530
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-09-09 19:51:57 +0530

    Medium level question

---
+++ MovieRating.sql
@@ -0,0 +1,86 @@
+-- Write a solution to:
+
+-- Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+-- Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Movies table:
+-- +-------------+--------------+
+-- | movie_id    |  title       |
+-- +-------------+--------------+
+-- | 1           | Avengers     |
+-- | 2           | Frozen 2     |
+-- | 3           | Joker        |
+-- +-------------+--------------+
+-- Users table:
+-- +-------------+--------------+
+-- | user_id     |  name        |
+-- +-------------+--------------+
+-- | 1           | Daniel       |
+-- | 2           | Monica       |
+-- | 3           | Maria        |
+-- | 4           | James        |
+-- +-------------+--------------+
+-- MovieRating table:
+-- +-------------+--------------+--------------+-------------+
+-- | movie_id    | user_id      | rating       | created_at  |
+-- +-------------+--------------+--------------+-------------+
+-- | 1           | 1            | 3            | 2020-01-12  |
+-- | 1           | 2            | 4            | 2020-02-11  |
+-- | 1           | 3            | 2            | 2020-02-12  |
+-- | 1           | 4            | 1            | 2020-01-01  |
+-- | 2           | 1            | 5            | 2020-02-17  | 
+-- | 2           | 2            | 2            | 2020-02-01  | 
+-- | 2           | 3            | 2            | 2020-03-01  |
+-- | 3           | 1            | 3            | 2020-02-22  | 
+-- | 3           | 2            | 4            | 2020-02-25  | 
+-- +-------------+--------------+--------------+-------------+
+-- Output: 
+-- +--------------+
+-- | results      |
+-- +--------------+
+-- | Daniel       |
+-- | Frozen 2     |
+-- +--------------+
+-- Explanation: 
+-- Daniel and Monica have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+-- Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.
+SELECT t1.name AS results
+FROM 
+(
+    SELECT 
+        Users.name,
+        COUNT(MovieRating.user_id) AS rating_count
+    FROM 
+        Users
+    JOIN 
+        MovieRating ON Users.user_id = MovieRating.user_id
+    GROUP BY 
+        Users.name
+    ORDER BY 
+        rating_count DESC, name ASC
+    LIMIT 1
+) AS t1
+
+UNION ALL
+
+SELECT t3.title AS result
+FROM 
+(
+    SELECT Movies.title 
+    FROM Movies
+    JOIN 
+    (
+        SELECT movie_id, SUM(rating) / COUNT(movie_id) AS avg_rating
+        FROM MovieRating
+        WHERE created_at BETWEEN "2020-02-01" AND "2020-02-29"
+        GROUP BY movie_id
+    ) AS t2 ON t2.movie_id = Movies.movie_id
+    ORDER BY t2.avg_rating DESC, Movies.title ASC
+    LIMIT 1
+) AS t3;
