d2fdc9c030c5386087bd947928a28e09127ceeb5
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-09-10 22:46:33 +0530

    DCL Commands revision

---
+++ DCL_Commands.sql
@@ -0,0 +1,6 @@
+-- Data Control Language is used to control access by the users to 
+-- GRANT 
+-- Assigns new privileges to a user account, allowing access to specific database objects, actions, or functions
+-- REVOKE
+-- Removes previously granted privileges from a user account, taking away their access to certain database objects or actions.
+
